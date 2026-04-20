dd75a54940fdf12c02b66a562d152b00f86eb628
Gakusyun <i@gkux.cn>
2026-04-20 22:25:33 +0800

    feat: 修改默认生产环境地址

--- Minipro-cli/src/app.ts
+++ Minipro-cli/src/app.ts
@@ -1,7 +1,7 @@
 import { createApp } from '@vue-mini/core';
 
 // API Base URL - 便于部署时修改不同环境
-export const BASE_URL = 'https://server.gxj62.cn/api/v1';
+export const BASE_URL = 'https://lms.gxj62.cn/api/v1';
 // 开发环境
 // export const BASE_URL = 'http://localhost:8000/api/v1';
 
